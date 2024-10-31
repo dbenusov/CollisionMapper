1bcd069fa92a3d1d2bd4276c2d9831eec6967f63
Daniel Benusovich <flyingmonkeys1996@gmail.com>
2024-10-30 22:15:19 -0700

    Udpates to get build passing.
    Signed-off-by: Daniel Benusovich <flyingmonkeys1996@gmail.com>

--- components/test-database-support/src/main/kotlin/TestDatabaseTemplate.kt
+++ components/test-database-support/src/main/kotlin/TestDatabaseTemplate.kt
@@ -3,6 +3,9 @@ package io.initialcapacity.collector
 import io.initialcapacity.DatabaseTemplate
 import org.jetbrains.exposed.sql.Database
 
+private val database_host = System.getenv("DATABASE_HOST") ?: "localhost"
+private val database_port = System.getenv("DATABASE_PORT") ?: "5432"
+
 fun testDatabaseTemplate(databaseName: String) = DatabaseTemplate(Database.connect(
-    url = "jdbc:postgresql://localhost:5432/${databaseName}_test?user=postgres&password=password"
+    url = "jdbc:postgresql://$database_host:$database_port/${databaseName}_test?user=postgres&password=password"
 ))
