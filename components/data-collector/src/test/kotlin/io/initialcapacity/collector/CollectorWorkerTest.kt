adb3060c7788e4b0716b1d233a9b62312edfd4b9
Daniel Benusovich <flyingmonkeys1996@gmail.com>
2024-11-03 16:06:37 -0800

    comments.
    Signed-off-by: Daniel Benusovich <flyingmonkeys1996@gmail.com>

--- components/data-collector/src/test/kotlin/io/initialcapacity/collector/CollectorWorkerTest.kt
+++ components/data-collector/src/test/kotlin/io/initialcapacity/collector/CollectorWorkerTest.kt
@@ -21,6 +21,7 @@ class CollectorWorkerTest {
     @Test
     fun testExecute() {
         val expected_data = listOf(CollisionData("10018", 32.52434444F, -86.672119440F, "2015"), CollisionData("10124", 32.58379167F, -86.464288890F, "2014"), CollisionData("10318", 32.64268056F, -86.756822220F, "2014"))
+        // This runs the worker with a fake data source. The fake returns the list of data passed into it.
         val worker = CollectorWorker(gateway, "test", FakeCollectorDataSource(expected_data))
         val task = CollectorTask("/crashes/GetCrashesByLocation?fromCaseYear=2014&toCaseYear=2015&state=1&county=1&format=json", false, CollectorMetrics("1234", "1234"))
         worker.execute(task)
