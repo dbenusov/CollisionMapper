9909dc9101c953a1dca4face86eaade498a7006b
Daniel Benusovich <flyingmonkeys1996@gmail.com>
2024-11-04 23:00:36 -0800

    Another production change...
    Signed-off-by: Daniel Benusovich <flyingmonkeys1996@gmail.com>

--- components/database-support/build.gradle.kts
+++ components/database-support/build.gradle.kts
@@ -5,6 +5,9 @@ val hikariVersion: String by project
 dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
+    // Used for Google SQL connections. Look in the Database configuration prudction
+    // setup.
+    implementation("com.google.cloud.sql:postgres-socket-factory:1.21.0")
 
     testImplementation("org.postgresql:postgresql:$postgresVersion")
 }
--- components/database-support/src/main/kotlin/io/initialcapacity/DatabaseConfiguration.kt
+++ components/database-support/src/main/kotlin/io/initialcapacity/DatabaseConfiguration.kt
@@ -7,19 +7,20 @@ import org.jetbrains.exposed.sql.Database
 private val database_host = System.getenv("DATABASE_HOST") ?: "localhost:5432"
 private val database_user = System.getenv("POSTGRES_USER") ?: "postgres"
 private val database_password = System.getenv("POSTGRES_PASSWORD") ?: "password"
-private val use_ssl = System.getenv("USE_SSL")?.toBoolean() ?: false
+private val is_prod = System.getenv("IS_PROD")?.toBoolean() ?: false
 
 class DatabaseConfiguration() {
-    private val url = "jdbc:postgresql://$database_host/collisions"
     private val config = HikariConfig().apply {
-        jdbcUrl = url
-        username = database_user
-        password = database_password
-        if (use_ssl) {
-            addDataSourceProperty("sslmode", "require")
-            // Set Cloud SQL instance as a property
-            addDataSourceProperty("cloudSqlInstance", "your-project-id:your-region:your-instance-name")
+        if (is_prod) {
+            jdbcUrl = "jdbc:postgresql:///collisions"
+            addDataSourceProperty("cloudSqlInstance", "collision-mappter:us-central1:collision-mapper-db")
             addDataSourceProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory")
+            addDataSourceProperty("user", database_user)
+            addDataSourceProperty("password", database_password)
+        } else {
+            jdbcUrl = "jdbc:postgresql://$database_host/collisions"
+            username = database_user
+            password = database_password
         }
     }
     private val ds = HikariDataSource(config)
